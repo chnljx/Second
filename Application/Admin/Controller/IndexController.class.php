006050eb3b56feb7f0e6cb419deef3dd11ecc18d
michael <chnljx@126.com>
2016-11-14 21:29:10 +0800

    用户

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,6 +3,12 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends AdminController {
     public function index(){
+        if(session('admin_user.name') == 'admin'){
+            $limit = "超级管理员";
+        }else{
+            $limit = M('role')->field('name')->where('id in'.M('user_role')->field('rid')->where('uid ='.session('admin_user.id'))->buildSql())->select();
+        }
+        $this->assign('limit',$limit['0']['name']);
         $this->display();
     }
 
