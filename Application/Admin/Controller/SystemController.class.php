b92ae1fa677a2f8b9bd6d69315aa9552c6c1c164
unknown <nick>
2016-11-16 22:53:59 +0800

    系统统计

--- Application/Admin/Controller/SystemController.class.php
+++ Application/Admin/Controller/SystemController.class.php
@@ -6,6 +6,10 @@ class SystemController extends AdminController
 {
     public function index()
     {
+        $types = M('type')->where('pid=0')->select();
+        foreach ($types as $v) {
+            
+        }
         // $types = M('bar')->field('t.name,count(b.id) count')->table('qm_type t,qm_bar b')->where('t.id=b.typeid and t.pid=0')->group('b.typeid')->select();
 
         // var_dump($types);
