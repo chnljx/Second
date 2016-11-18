9c8753928d071ae46edbc465d56692411cabfb8b
unknown <348851371@qq.com>
2016-11-18 16:01:37 +0800

    lunbo

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,8 +5,7 @@ class IndexController extends HomeController {
     public function index(){
         $this->assign('title','首页');
         if(!empty($_SESSION['home_user'])){
-        	$test1="退出";
-            $test2="";
+
             $mes="我在贴吧";
             $mes1="注册时间:";
             $mes2="爱逛的吧:";
@@ -17,22 +16,14 @@ class IndexController extends HomeController {
             if(empty($data)){
                 $emp="你还没有常逛的吧，快去找找吧>>>>";
             }
-            // var_dump($data);
-
-        }else{
-        	$test1="";
-            $test2="登录";
         }
-        $this->assign('test1',$test1);
-        $this->assign('test2',$test2);
-        $this->assign('url',$url);
+        
         $this->assign('mes',$mes);
         $this->assign('mes1',$mes1);
         $this->assign('mes2',$mes2);
+        $this->assign('emp',$emp);
 
         $this->assign('list',$data);
-        $this->assign('emp',$emp);
-        // var_dump($_SESSION['home_user']);
         $this->display();
     }
 }
