974346e7aac35695d922ce7c3b4586c892ce6278
unknown <nick>
2016-11-21 23:08:28 +0800

    贴吧遍历

--- Application/Home/Controller/BarController.class.php
+++ Application/Home/Controller/BarController.class.php
@@ -1,8 +1,55 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class BarController extends HomeController {
-    public function index(){
+
+/**
+*BarController 贴吧控制器
+* @author xiao
+* @version 1.0
+*/
+class BarController extends HomeController 
+{
+    public function index()
+    {
+        // 图片轮播
+        $data = M('picture')->where('bid=46')->limit(12)->select();
+        // 贴吧名
+        $bar = M('bar')->field('b.name, b.picname, b.id, u.name uname, b.descr, t.name tname')->table('qm_bar b,qm_user u, qm_type t')->where('b.id=46 and b.uid=u.id and t.id=b.typeid')->find();
+        // 关注人数
+        $follow = M('follow')->where('bid=46')->select();
+        // 帖子
+        $list = M('post')->field('u.name uname, p.title, p.descr, p.state, p.ctime, p.id')->table('qm_user u,qm_post p')->where('p.bid=46 and p.state=1 and p.uid=u.id')->order('p.ctime desc')->page($_GET['p'],5)->select();
+        // 分页
+        $count = M('post')->table('qm_user u,qm_post p')->where('p.bid=46 and p.state=1 and p.uid=u.id')->count();
+
+        $Page = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数
+        $show = $Page->show();// 分页显示输出
+        $this->assign('page',$show);
+
+        // 贴吧名人
+        $supfans = M('follow')->field('u.name, u.exp, u.picname')->table('qm_user u, qm_follow f')->where('f.bid=46 and f.uid=u.id')->order('u.exp desc')->select();
+
+
+        foreach ($list as $k=>$v) {
+            // 收藏
+            $num = M('collection')->where('postid='.$v['id'])->select();
+            // 回复
+            $nums = M('comment')->where('postid='.$v['id'])->select();
+            $pics = M('post_photo')->field('picname')->where('postid='.$v['id'])->limit(1)->select();
+            $list[$k]['count']=count($num);
+            $list[$k]['comcount']=count($nums);
+            $list[$k]['pics']=$pics[0]['picname'];
+            $list[$k]['picnum']=count($pics);
+
+        }
+
+        $this->assign('datas',$data);
+        $this->assign('list',$list);
+        $this->assign('num',$count);
+        $this->assign('bar',$bar);
+        $this->assign('follow',count($follow));
+        $this->assign('supfans',$supfans);
         $this->display();
     }
+    
 }
--- Application/Home/Controller/PostController.class.php
+++ Application/Home/Controller/PostController.class.php
@@ -2,10 +2,75 @@
 namespace Home\Controller;
 use Think\Controller;
 
+/**
+*PostController 帖子控制器
+* @author xiao
+* @version 1.0
+*/
 class PostController extends HomeController
 {
 	public function index()
 	{
+        $arr = M('post')->field('b.name bname, b.picname bpic, u.name uname, u.picname upic, u.exp, p.title, p.descr, p.ctime, p.id')->table('qm_user u, qm_bar b, qm_post p')->where('p.id='.I('get.id').' and u.id=p.uid and b.id=p.bid')->find();
+        // 评论
+        $comment = M('comment')->field('u.name name, u.picname upic, u.exp, u.id uid, c.ctime, c.content')->table('qm_user u, qm_comment c')->where('c.postid='.I('get.id').' and c.uid=u.id and c.state=1')->page($_GET['p'],5)->select();
+        // 分页
+        // $count = M('comment')->field('u.name name, u.picname upic, u.id uid, c.ctime, c.content')->table('qm_user u, qm_comment c')->where('c.postid='.I('get.id').' and c.uid=u.id and c.state=1')->select();
+
+        // $Page = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数
+        // $show = $Page->show();// 分页显示输出
+        $this->assign('page',$show);
+        $this->assign('comment',$comment);
+        $this->assign('arr',$arr);
 		$this->display();
 	}
+
+    // 添加帖子
+    public function doAdd()
+    {
+        $data = $_POST;
+        $post = D("Post"); // 实例化User对象
+        if (!$post->create($data)){ 
+            $this->error($post->getError());
+        }else{
+
+            // 执行添加
+            if ($post->add() > 0) {
+                $this->success('添加成功', U('Bar/index'));
+            } else {
+                $this->error('添加失败');
+            }   
+        }
+    }
+
+    // public function upload() 
+    // {
+        /*$config = array(
+            'maxSize' => 3145728,
+            'rootPath' => './Upload/img/post/',
+            'saveName' => array('uniqid',''),
+            'exts' => array('jpg', 'gif', 'png', 'jpeg'),
+            'autoSub' => true,
+            'subName' => array('date','Ymd'),
+        );
+        $upload = new \Think\Upload($config);// 实例化上传类
+        // 上传单个文件
+        $info = $upload->uploadOne($_FILES['picname']);
+        if(!$info) {// 上传错误提示错误信息
+            $this->error($upload->getError());
+        } else {// 上传成功 获取上传文件信息
+            $path = $info['savepath'].$info['savename'];
+            $image = new \Think\Image();
+            $image->open("./Upload/img/post/".$path);
+            // 按照原图的比例生成一个最大为90*90的缩略图并保存为thumb.jpg
+            $path = time().$info['savename'];
+            $image->thumb(100, 100)->save('./Upload/img/post-thumb/'.$path);
+        }*/
+    //     $data = [
+    //         'code' => 0,
+    //         'msg' => '1',
+    //         'data' => ['src' => '1','title' => '1'],
+    //     ];
+    //     $this->ajaxReturn($data);
+    // }
 }
