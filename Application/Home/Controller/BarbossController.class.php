33fda02bd081aa25d54308b08377bcc71254b0be
unknown <nick>
2016-11-24 22:31:59 +0800

    贴吧管理

--- Application/Home/Controller/BarbossController.class.php
+++ Application/Home/Controller/BarbossController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use \Think\Controller;
 
 /**
-* BarbossController 用户资料
+* BarbossController 贴吧资料
 *
 * @author michael
 * @version 1.0
@@ -35,50 +35,117 @@ class BarbossController extends HomeController
 
     public function index()
     {   
-        $bar = M('bar')->where('id='.I('get.id'))->find();
+        $bar = M('bar')->field('t.name tname, b.id, b.name, b.descr, b.picname, b.ctime, b.state')->table('qm_bar b, qm_type t')->where('b.typeid=t.id and b.id='.I('get.id'))->find();
+        $follow = M('Follow')->field('u.name, u.picname, u.exp, u.descr, u.regtime')->table('qm_follow f, qm_user u')->where('f.bid='.I('get.id').' and f.uid=u.id and u.state=1')->order('u.exp desc')->page($_GET['p'],10)->select();
+
+        $count = M('Follow')->table('qm_follow f, qm_user u')->where('f.bid='.I('get.id').' and f.uid=u.id and u.state=1')->count();
+        $Page = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数
+
+        $Page->setConfig('first','首页');
+        $Page->setConfig('last','尾页');
+        $Page->setConfig('prev','上一页');
+        $Page->setConfig('next','下一页');
         
-        // $post_count = M('Post')->where('uid='.session('home_user.id'))->count("id");
-        $follow = M('Follow')->field('u.name, u.picname, u.exp')->table('qm_follow f, qm_user u')->where('f.bid='.I('get.id').' and f.uid=u.id and u.state=1')->order('u.exp desc')->select();
+        $Page->setConfig('theme','
+            <nav>
+              <ul class="pagination">
+                <li>%FIRST%</li>
+                <li>%UP_PAGE%</li>
+                <li>%LINK_PAGE%</li>
+                <li>%DOWN_PAGE%</li>
+                <li>%END%</li>
+              </ul>
+            </nav>
+        ');
 
-        // session('home_user.post_count', $post_count);
-
-        // $this->assign('post_count', $post_count);
+        $show = $Page->show();// 分页显示输出
+        $this->assign('page',$show);
+        $this->assign('count',$count);
 
         $this->assign('bar', $bar);
         $this->assign('follow', $follow);
         $this->display();
     }
 
-    /*
-        个人资料更新
-    */ 
-    // public function editview()
-    // {
-    //     $User = M('User')->where('id='.session('home_user.id'))->find();
-    //     $this->assign('user',$User);
-    //     $this->display('User:edit');
-    // }
+    /**
+    * 修改贴吧信息页面
+    * @access public        
+    * @return void
+    */
+    public function editView()
+    {
+        $bar = M('bar')->field('t.name tname, b.id, b.name, b.descr, b.picname, b.ctime, b.state')->table('qm_bar b, qm_type t')->where('b.typeid=t.id and b.id='.I('get.id'))->find();
+    
+        $list = M('bar')->field('u.name uname, t.name tname, b.id, b.name, b.descr, b.picname, b.ctime, b.state')->table('qm_bar b, qm_type t, qm_user u')->where('b.typeid=t.id and b.uid=u.id and b.id='.I('get.id'))->find();
+        $this->assign('list',$list);
+        $this->assign('bar', $bar);
+        $this->display('Barboss/edit');
 
-    // public function save()
-    // {
-    //     $User = D("User"); // 实例化User对象
-    //     if (!$User->create()){
-    //         // 如果创建失败 表示验证没有通过 输出错误提示信息
-    //         $this->error($User->getError());
-    //     }else{
-    //         // 验证通过 可以进行其他数据操作
-    //         $id = I('post.id');
-    //         $map['sex'] = I('post.sex');
-    //         $map['code'] = I('post.code');
-    //         $map['birthday'] = strtotime(I('post.birthday'));
-    //         $map['descr'] = I('post.descr');
-    //         if($User->where("id=".$id)->save($map)){
-    //             $this->success('修改成功');
-    //         }else{
-    //             $this->error('修改失败');
-    //         }
-    //     }
-    // }
+    }
+
+    /**
+    * 执行修改贴吧
+    * @access public        
+    * @return void
+    */
+    public function save()
+    {
+        //得到数据模型
+        $bar = D('Barboss');
+        $data = $__POST;
+        //过滤数据,数据验证
+        if (!$bar->create($data)) {
+            // 如果创建失败 表示验证没有通过 输出错误提示信息
+            if(IS_AJAX){   
+                $this->ajaxReturn($bar->getError());
+            }else{
+                $this->error($bar->getError());
+            }
+               
+        } else {
+            // 验证通过 可以进行其他数据操作
+            if(!IS_AJAX){
+                // 验证通过 可以进行其他数据操作
+                if($_FILES['picname']['name'] != ''){
+                    $config = array(
+                        'maxSize' => 3145728,
+                        'rootPath' => './Upload/img/tieba/',
+                        'saveName' => array('uniqid',''),
+                        'exts' => array('jpg', 'gif', 'png', 'jpeg'),
+                        'autoSub' => true,
+                        'subName' => array('date','Ymd'),
+                    );
+                    $upload = new \Think\Upload($config);// 实例化上传类
+                    // 上传单个文件
+                    $info = $upload->uploadOne($_FILES['picname']);
+                    if(!$info) {// 上传错误提示错误信息
+                        $this->error($upload->getError());
+                    } else {// 上传成功 获取上传文件信息
+                        $path = $info['savepath'].$info['savename'];
+                        $image = new \Think\Image();
+                        $image->open("./Upload/img/tieba/".$path);
+                        // 按照原图的比例生成一个最大为90*90的缩略图并保存为thumb.jpg
+                        $path = time().$info['savename'];
+                        $image->thumb(100, 100)->save('./Upload/img/tieba-thumb/'.$path);
+                    }
+                    
+                }
+                if($path != ''){
+                    $data['picname'] = $path;   
+                }
+                
+                
+                $data['descr'] = I('post.descr');
+                // 执行添加
+                if ($bar->where('id='.I('post.id'))->save($data) > 0) {
+                    $this->success('修改成功', U('editView').'?id='.I('post.id'));
+                } else {
+                    $this->error('修改失败');
+                }   
+            }
+        }
+    }
+
 
     // 密码显示
     // public function pwdview()
