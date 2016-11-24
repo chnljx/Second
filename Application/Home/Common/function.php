f22838a4d0870610d88ecdc081ebafbc9ccc6a86
michael <chnljx@126.com>
2016-11-24 11:13:13 +0800

    界面

--- Application/Home/Common/function.php
+++ Application/Home/Common/function.php
@@ -4,3 +4,15 @@ function barage($param)
 {   
     return date('Y') - date('Y', $param);
 }
+
+// 贴子时间显示
+function post_time($param)
+{
+    if (date('Y', $param) != date("Y")) {
+        return date('Y-m-d H:i', $param);
+    } elseif (date('Y-m-d', $param) == date("Y-m-d")) {
+        return date('H:i:s', $param);
+    } else {
+        return date('m-d H:i', $param);
+    }
+}
