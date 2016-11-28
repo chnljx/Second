bb6ff57231b994fae74739b8a67a0d8cb7980421
unknown <nick>
2016-11-28 09:52:22 +0800

    new

--- Application/Home/Model/CommentModel.class.php
+++ Application/Home/Model/CommentModel.class.php
@@ -6,11 +6,11 @@ class CommentModel extends Model{
     
     protected $_validate = array(
         // array(验证字段2,验证规则,错误提示,[验证条件,附加规则,验证时间])
-        array('comment','require','请输入发表的内容'),
+        array('content','require','请输入发表的内容'),
         
     );
 
-     protected $_auto = array (
+    protected $_auto = array (
         // array(完成字段1,完成规则,[完成条件,附加规则]),
         array('ctime', 'time', 3, 'function'), // 对ctime字段写入当前时间戳
     );
